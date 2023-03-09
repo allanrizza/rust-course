4f4ab5858f9723bbca50fb5a14ceebce659c60f2
Allan Rizza <allanrizza.dev@gmail.com>
2023-03-09 00:08:36 -0300

    Using pause method

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 #[path = "../section2/src/section2.rs"] mod section2;
+mod pause;
 fn main() {
     section2::section_2_fn();
+
+    // The pause method is used to prevent the program from closing immediately after executing the executable file. The pause method waits for user input before closing the terminal window, allowing enough time to read any output or error messages that may be displayed before the program exits.
+    pause::pause();
 }
